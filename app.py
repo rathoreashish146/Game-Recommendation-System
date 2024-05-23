0d8f4e589c870978f5838aa358905581dc6aeca2
Ashish Rathore <rathoreashish146@gmail.com>
2024-05-23 15:48:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -27,5 +27,4 @@ def chat():
     return str(result)
 
 if __name__ == '__main__':
-    from waitress import serve
-    serve(app, host="0.0.0.0", port=8080)
+    app(host="0.0.0.0", port=8080,debug=True)

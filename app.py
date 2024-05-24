c7eec10f843b3b31081f0b8a92e6af0b970364c8
Ashish Rathore <rathoreashish146@gmail.com>
2024-05-24 13:52:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -26,5 +26,6 @@ def chat():
     print("Response : ", result)
     return str(result)
 
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=8080,debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
